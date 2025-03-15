1e6a9b14ed6a0e2437a972fa0c9757f96d93b070
Le-Maz <mazurlech@proton.me>
2025-03-15 15:19:43 +0100

    Create base services

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use injector::{Injected, Injector};
+use injector_macro::Injectable;
+
+#[derive(Injectable)]
+pub struct ConfigService {}
+
+#[derive(Injectable)]
+pub struct TuiService {}
+
+#[derive(Injectable)]
+pub struct TunService {}
+
+#[derive(Injectable)]
+pub struct IrohService {}
+
+#[derive(Injectable)]
+pub struct AppService {
+    config_service: Injected<ConfigService>,
+    tui_service: Injected<TuiService>,
+    tun_service: Injected<TunService>,
+    iroh_service: Injected<IrohService>,
+}
+
+impl AppService {
+    pub fn run(&self) {}
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut injector = Injector::default();
+    injector.get::<AppService>().run();
 }
